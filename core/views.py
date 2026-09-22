d9f1dac881f8a2d7c38bb47dc6e7d19eac54feeb
Sakhawat Ali <msakhawatali131@gmail.com>
2026-09-22 18:51:38 +0500

    create register and login method

--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models import Sum, Count, Q
+from django.http import JsonResponse
+from django.utils import timezone
+from datetime import timezone
+from decimal import Decimal
+from .models import *
+from .forms import *
+import json
 
 # Create your views here.
+# Authentication viewa
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successfull! welcome to Transport Management System.')
+
+            return redirect('dashboard')
+        else:
+            form = UserRegistrationForm()
+        return  render(request, 'core/register.html', {'form' : form})
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        form = LoginForm(request, data= request.POST)
+        if form.is_vaild():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('dashboard')
+        messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'core/login.html', {'form': form})
